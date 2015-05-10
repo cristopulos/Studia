adf4d61e18b0d94ef19104d6b449c2814c3d23f7
Krzysztof Heldt <cristopulos1995@gmail.com>
2015-05-10 17:52:14 +0200

    ASm 2

--- assembler/Zad2.asm
+++ assembler/Zad2.asm
@@ -1,18 +1,28 @@
 	.data
-cyfra:	.asciiz "\npodaj ilosc cyfr dzielnej :"
-dziel1: .asciiz "\npodaj wartosc dzielnej :"
-dziel2:	.asciiz "\npodaj wartosc dzielnika :"
+cyfra:	.asciiz "\npodaj ilosc cyfr dzielnej : "
+dziel1: .asciiz "\npodaj wartosc dzielnej : "
+dziel2:	.asciiz "\npodaj wartosc dzielnika : "
 exc:	.asciiz "dzielna moze skladac sie tylko z cyfr od 0 do 9"
-result: .asciiz "wynik to :"
-result2:.asciiz "reszta wynosi :"
+exc2:	.asciiz "dzielnik nie moze byc zerem"
+exc3:	.asciiz "ilosc cyfr dzielnej musi byc wieksza od zera"
+result: .asciiz "wynik to : "
+result2:.asciiz "\nreszta wynosi : "
 
 	.text
-	la $a0,cyfra
+#-----------------------MAIN------------------------------#
+start:	la $a0,cyfra
 	li $v0, 4
 	syscall 	# wypisanie komunikatu
 	li $v0, 5
 	syscall 	# wczytanie int-a
 	move $s3, $v0	# $s3 - ilosc cyfr do wpisania
+	
+	bgtz $s3, else	# jezeli uzytkownik wpisal w pole ilosci cyfr liczbe mniejsza od zera 
+	la $a0 ,exc3	# wyswietl stosowna informacje
+	li $v0,4
+	syscall
+	j start		# i zakoncz program
+else:	
 	addi $v0,$v0,2
 	move $t1,$v0
 	move $a0,$v0	# wczytanie argmentow do metody
@@ -32,87 +42,124 @@ result2:.asciiz "reszta wynosi :"
 	li $v0, 8
 	syscall		# pobranie dzielnej jako ciag znakow
 	
-	la $a0,dziel2
+start2:	la $a0,dziel2
 	li $v0, 4
-	syscall
+	syscall		# wypisanie prosby o wpisaniu dzielnika
 	li $v0, 5
 	syscall		# wczytanie dzielnika
 	move $s1,$v0 	# dzielnik w rejestrze $s1
-	
+	bnez $s1, notZero
+	la $a0,exc2
+	li $v0, 4
+	syscall 	# wypisanie komunikatu o blednie wprowadzonych danych
+	j start2
+notZero:	
 	move $a0,$s0
 	move $a1,$s1
 	move $a2,$s2
 	move $a3,$s3
 	jal division 	# $a0 - dzielna, $a1 - dzielnik, $a2 adres na pamiec na wynik, $a3 ilosc cyfr dzielnika
-	move $v0,$s0	# $s0 - adres na pamiec wyniku
-	move $v1,$s1	# $s1 - reszta z dzielenia
+	move $s0,$v0	# $s0 - adres na pamiec wyniku
+	move $s1,$v1	# $s1 - reszta z dzielenia
 	la $a0,result
 	li $v0, 4
 	syscall		# wypisanie komunikatu
-	move $a0,$s0
+	bnez $s0, string
+	li $v0,1
+	li $a0,0
+	syscall
+	j goOn
+string:	move $a0,$s0
+	li $v0,4
 	syscall		# wypisanie wyniku
-	la $a0,result2	
+	
+goOn:	la $a0,result2	
+	li $v0,4
 	syscall		# wypisanie drugiego komunikatu
 	move $a0,$s1
 	li $v0,1
 	syscall
-	
-	
-	
-	
+		
 out:	li $v0, 10
 	syscall
 	
-
+#-----------------------------------F-cja dopelniajaca liczbe do wielkrotnosci 4----------------------------------------#
 	
 dopDo4:
-	addi, $sp,$sp, -8
-	sw $t0,0($sp)
-	sw $t1,4($sp)
+	addi, $sp,$sp, -8	# odpowiednie ustawienie wskaznika konca stosu
+	sw $t0,0($sp)		# |
+	sw $t1,4($sp)		# upewniamy sie ze nie starcimy zadnych danych
 	li $t0,4
-	div $v0,$t0
-	mfhi $t1
-	beqz $t1,return
+	div $v0,$t0		
+	mfhi $t1		# reszta z dzielnia przez 4
+	beqz $t1,return		# jezeli liczba juz jest wielokrotnoscia 4 nie dodawaj nic
 	sub $t0,$t0,$t1
-	add $v0,$a0,$t0
-return:	lw $t0,0($sp)
-	lw $t1,4($sp)
+	add $v0,$a0,$t0		# dopelnienie do nast. wielokrotnosci 4
+return:	lw $t0,0($sp)		# |
+	lw $t1,4($sp)		# wczytanie wczesniej zapisanych danych
 	addi $sp,$sp,8
 	jr $ra
 
+#-------------------------------------F-cja odpowiedzialna za dzielenie liczb----------------------------------------------------#
 division:
-	addi $sp,$sp,-20
-	sw $s0,0($sp)
-	sw $s1,4($sp)
-	sw $s2,8($sp)
-	sw $s3,12($sp)		# upewniamy sie ze nie stracimy zadnych danych
-	sw $s4, 16($sp)
+#----Poczatkowe operacje----#
+	addi $sp,$sp,-44	# odpowiednie przesuniecie znacznika konca stosu
+	sw $s0,0($sp)		# |
+	sw $s1,4($sp)		# |
+	sw $s2,8($sp)		# |
+	sw $s3,12($sp)		# |
+	sw $s4, 16($sp)		# |
+	sw $s5, 20($sp)		# |
+	sw $s6, 24($sp)		# upewniamy sie ze nie stracimy zadnych danych
+	sw $s7, 28($sp)		# |
+	sw $t0,	32($sp)		# |
+	sw $t3, 36($sp)		# |
+	sw $t2, 40($sp)		# |
+	sw $t5, 44($sp)		# |
 	
 	move $s0,$a0		# $t0 : adres na poczatek pamieci dzielnej
 	move $s1,$a3		# $t1 : ilosc cyfr dzielnej
+	move $s6,$a2		# $s6 : adres w pamieci gdzie zaczyna sie wynik
 	
-	li $v0,0
-	li $v1,0		# zerowanie wynikow
 	li $s3,0 		# flaga wskazuj¹ca, czy dzielna jest ujemna
+	li $s7,0		# flaga wskazujaca, czy dzielnik jest ujemny
+	li $s5,0		# flaga wskaujaca ze rozpoczelo sie dzielenie
 	
 	lbu $t2,0($s0) 		# wczytanie pierwszego znaku
-	subi $s1,$s1,1
-	beqz $t2,return2
+	subi $s1,$s1,1		# zmniejszenie ilosci cyfr do wczytania o jeden
 	subi $t2,$t2,48 	# zmiana kodu ascii na liczbe
-	li $t3,-3
-	bne $t2,$t3,positive 	# je¿eli pierwszy znak to minus [ascii : 45]
-	addi $s3,$s3,1	 	# zmiana flagi na "true"
-	addi $s1,$s1,1		
-positive:
-	bnez $s3,next
- 	div $t0,$t2,10
- 	bnez $t0,exception	# jezeli wczytany znak nie jest liczba wyrzuc komunikat
-	move $s4,$t2 		# je¿eli pierszy znak byl liczba wczytaj do rejestru przechowujacego wartosc dzielnej
+	li $t3,-3		# kod ascii znaku minus 45 : 45-48 = -3
+	
+	bne $t2,$t3,brn5 	# je¿eli pierwszy znak to nie minus [ascii : 45] -> dzielna jest dodatnia
+	li $s3,1	 	# zmiana flagi dzielnej na "true"
+	
+brn5:	bgtz $a1,brn6		# jezeli dzielnik jest wiekszy od zera
+	li $s7,1		# zamiana flagi dzielnika na "true"
+	
+brn6:	beq $s3,$s7, brn7	# jezeli znaki dzielnika i dzielnej sa takie same nie ma potrzeby dopisywania minusa	
+	li $t2,45		# kod ascii 45 -> znak minus
+	sb $t2,0($a2)		# wypisanie minusa na poczatek
+	addi $a2,$a2,1		# przesuniecie znacznika na adres w pamieci o jeden bajt w prawo
+	
+brn7:	beqz $s7,brn2		# jezeli dzielnik jest liczba ujemna -> flaga jest ustawiona na true w $s7
+	sub $a1,$zero,$a1	# zamiana dzielnika na liczbe dodatnia
+brn2:	
+	beqz $s3, brn3		# jezeli dzielna byla ujemna -> miala na pocztaku minus
+	addi $s1,$s1,1		# dodanie jedynki do licznika pozostalych cyfr gdyz zczytany znak nie byl cyfra a minusem
+brn3:
+	bnez $s3,next		# jezeli wczytany znak byl minusem przeskocz dalej
+ 	div $t0,$t2,10		# sprawdzamy, czy to co wczytalismy jest cyfra
+ 	bnez $t0,exception	# jezeli wczytany znak nie jest cyfra wyrzuc komunikat
+	move $s4,$t2 		# je¿eli pierszy znak byl cyfra wczytaj ja do rejestru przechowujacego wartosc dzielnej
+	
+#----Wlasciwa petla dzialaca----#
 next:	addi $s0,$s0,1 		# zmiana adresu na nastepny znak do pobrania
+	bnez $s5,brn		# jezeli rozpoczelo sie juz dzielnie nie ma potrzeby dalej sprawdzac czy liczba reprez. dzielna jest wieksza od dzielnika
 	blt  $s4,$a1,cnt 	# jezeli wartosc dzielnej jest >= od dzielnika przystap do dzielenia
-	div $s4,$a1
-	mflo $t5
-	addi $t5,$t5,48 	# zamiana liczby z powrotem na kod ascii
+	li $s5,1		# ustawienie flagi na 'dzielenie rozpoczelo sie'
+brn:	div $s4,$a1		# dzielenie
+	mflo $t5		# przeniesienie wyniku do rejestru w celu zapisania go
+	addi $t5,$t5,48 	# zamiana cyfry z powrotem na kod ascii
 	sb $t5, 0($a2) 		# zapisanie znaku w pamieci przeznaaczonej na wynik
 	addi $a2,$a2,1 		# przesuniecie wskaznika o 1 bajt w prawo
 	mfhi $s4 		# przeniesienie reszty z powrotem do rejestru dzielnej
@@ -121,28 +168,33 @@ cnt:	beqz $s1, end		# jezeli nie ma juz cyfr do wczytania, zakoncz
 	lbu $t2, 0($s0)		# wczytanie kolejnego znaku dzielnej
 	subi $s1,$s1,1		# zminiejszenie ilosci pozostalych do pobrania cyfr o jeden
 	subi $t2,$t2,48		# zamiana znaku na liczbe
-	div $t0,$t2,10
-	bnez $t0,exception	# jezeli wczytany znak nie jest liczba wyrzuc kominukat
+	div $t0,$t2,10		# sprawdzenie, czy wczytany znak jest cyfra
+	bnez $t0,exception	# jezeli wczytany znak nie jest cyfra wyrzuc kominukat
 	add $s4,$t2,$s4		# dodanie tej liczby do zmiennej dla dzielnej
 	j next
-
-
-
-
-end : 	lw $s0,0($sp)
-	lw $s1,4($sp)
-	lw $s2,8($sp)
-	lw $s3,12($sp)
-	lw $s4,16($sp)
-	addi $sp,$sp,20		# wczytanie wartosci w rejestrach sprzed wywolania metody
-	move $v0,$a3		# $v0 : adres pamieci zawierajacej dzielna
+#----zakonczenie f-cji dzielacej----#
+end : 	move $v0,$s6		# $v0 : adres pamieci zawierajacej dzielna
 	move $v1,$s4		# $v1 : reszta z dzielenia
-return2:jr $ra 
-	
+	bnez $s5,brn8		# jezeli dzielenie wcale nie nastapilo 
+	li $v0,0		# ustawiamy rejestr wynikowy na zero zamiast na adres w pamieci
+brn8:	lw $s0,0($sp)		# |
+	lw $s1,4($sp)		# |
+	lw $s2,8($sp)		# |
+	lw $s3,12($sp)		# |
+	lw $s4,16($sp)		# |
+	lw $s5, 20($sp)		# wczytanie wartosci w rejestrach sprzed wywolania metody
+	lw $s6, 24 ($sp)	# |
+	lw $s7, 28($sp)		# |
+	lw $t0,	32($sp)		# |
+	lw $t3, 36($sp)		# |
+	lw $t2, 40($sp)		# |
+	lw $t5, 44($sp)		# |
+	addi $sp,$sp,44		# odpowiednie przesuniecie wskaznika konca stosu
+	jr $ra 			# zakonczenie metody/f-cji
+			
+#-----Wyjatek do f-cji dzielacej-----#			
 exception:
-	la $t0,exc
+	la $a0,exc		
 	li $v0, 4
-	syscall
-	lw $t0 , 0($sp)
-	addi $sp,$sp,4
-	j out
+	syscall			# wypisanie odpowiedniego komunikatu o bledzie
+	j out			# zkonczenie programu
